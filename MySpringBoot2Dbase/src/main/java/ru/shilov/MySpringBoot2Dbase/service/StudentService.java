f9e55c1033328d4189ced0200078209bc51e29ac
Evgeniy <Shilovevgeniy@inbox.ru>
2024-09-30 14:19:28 +0500

    (lab6)

---
+++ MySpringBoot2Dbase/src/main/java/ru/shilov/MySpringBoot2Dbase/service/StudentService.java
@@ -0,0 +1,17 @@
+package ru.shilov.MySpringBoot2Dbase.service;
+
+import org.springframework.stereotype.Service;
+import ru.shilov.MySpringBoot2Dbase.entity.Student;
+
+import java.util.List;
+
+@Service
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudent(int id);
+
+    void deleteStudent(int id);
+}
